6e60d8538124030454d475aa25c79b5ca2608ed1
Anupam Tamrakar <124962476+ANUPAM211@users.noreply.github.com>
2024-02-21 15:34:57 +0530

    push

---
+++ src/component/body.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+export default function body() {
+  return (
+    <div>
+    <form action="" >
+<input type="text" placeholder='First Name' />
+<input type="text" placeholder='Last Name' />
+<input type="Number" placeholder='Number' />
+<input type="text" placeholder='Address' />
+<input type="" placeholder='First Name' />
+<input type="text" placeholder='First Name' />
+<input type="text" placeholder='First Name' />
+<input type="text" placeholder='First Name' />
+
+
+
+    </form>
+    </div>
+  )
+}
